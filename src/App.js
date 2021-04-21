f4aa72ad0bceea830fdc6b7f817ee786a50ba265
Kayla Newlon <kayla.b.newlon@gmail.com>
2021-04-21 00:27:43 -0700

    set form state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [loginFormState, setLoginFormState] = useState({
+    email:"",
+    password:"",
+  })
+
+  const inputChange = event=>{
+    const {name, value}=event.target;
+    setLoginFormState({
+      ...loginFormState,
+      [name]:value
+    })
+  }
+
   return (
     <div className="App">
-     <div>"Minor Change"</div>
+     <form>
+       <input onChange={inputChange} value={loginFormState.email} type="text" name="email" placeholder="email" />
+       <input onChange={inputChange} value={loginFormState.password} type="password" name="password" />
+       <input type="submit" value="login" />
+     </form>
     </div>
   );
 }
